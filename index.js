e321d6bf7fb6462fe0120bcd93cde3a43aebbe81
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-03-30 21:58:47 +0600

    Node Mongo Recap Done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); // directly isn't access to this cors cause is security effected>>
 
 const app = express();
 app.use(cors());
